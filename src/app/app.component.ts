68889f7ba016c6574ec72094717fdf093dda81bd
MayaPorto <mayamiresporto@gmail.com>
2022-10-20 12:47:41 -0300

    credit-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartaoDeCredito } from './interfaces/CartaoDeCredito';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'credit-card';
+  formCartaoDeCredito: CartaoDeCredito = {
+    ano: '',
+    cvc: '',
+    mes: '',
+    nome: '',
+    numero: ''
+  }
 }
